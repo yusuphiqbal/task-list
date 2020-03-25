4c455da770df3440af9f7dba8387278a3ebcb5de
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-03-25 18:54:39 +0300

    Created addTask function that adds the user's tasks to the user interface

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearButton = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+loadEventlisters();
+
+function loadEventlisters() {
+  form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="material-icons">delete</i>';
+
+  li.appendChild(link);
+  taskList.appendChild(li);
+
+  taskInput.value = '';
+
+  e.preventDefault();
+}
